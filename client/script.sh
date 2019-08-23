9316c42eda71864b7113ca0290ca21ce4872acbd
Ishwariya Raveendran <48996604+ishriya@users.noreply.github.com>
2019-08-23 15:08:09 +0800

    Update script.sh

--- client/script.sh
+++ client/script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -H Host:whoami.docker.localhost http://172.20.0.7/ &&
-ab -n 500 -c 10 -H Host:whoami.docker.localhost http://172.20.0.7/ > traefik_test1.txt 
+ab -n 100 -c 5 -H Host:whoami.docker.localhost http://172.20.0.7/ > traefik_test1.txt 
 exec "$@";
