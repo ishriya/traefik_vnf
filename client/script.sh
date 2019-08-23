ca8b020906fefa70a037127947e7281012c1cb0f
Ishwariya Raveendran <48996604+ishriya@users.noreply.github.com>
2019-08-23 14:55:00 +0800

    Update script.sh

--- client/script.sh
+++ client/script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-curl -H Host:machine-echo.example.com http://172.20.0.4/ &&
-ab -n 5000 -c 10 -H Host:machine-echo.example.com http://172.20.0.4/ > traefik_test1.txt &&
-curl -H Host:echo-echo.example.com http://172.20.0.4/  &&
-ab -n 5000 -c 15 -H Host:echo-echo.example.com http://172.20.0.4/ > traefik_test2.txt
+curl -H Host:whoami.docker.localhost http://172.20.0.7/ &&
+ab -n 5000 -c 10 -H Host:whoami.docker.localhost http://172.20.0.7/ > traefik_test1.txt 
 exec "$@";
-         
